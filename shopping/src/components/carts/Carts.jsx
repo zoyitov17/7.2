347faa50b037ceb8ceaa6e3d30b85f64cc34d911
zoyitov17 <adhamjonzokhidov@gmail.com>
2024-06-23 23:52:21 +0500

    i

--- shopping/src/components/carts/Carts.jsx
+++ shopping/src/components/carts/Carts.jsx
@@ -1,28 +1,23 @@
 import React from "react";
-import { Data } from "../../data/dada"; 
+import { Data } from "../../data/dada";
 
 const Carts = () => {
   return (
-    <div className="grid grid-cols-3 gap-4">
-      {" "}
+    <div className="grid grid-cols-3 gap-2">
       {Data.map((item, index) => (
-        <div key={index} className="p-4 border rounded-md shadow-md">
-          {" "}
-          <div className="flex justify-center">
-            <img
-              src={item.image}
-              alt={item.common_name}
-              className="w-64 h-48 object-contain"
-            />{" "}
+        <div
+          key={index}
+          className="w-[330px] h-[250px] border border-black rounded-[20px]"
+        >
+          <div className="">
+            <img src={item.image} alt="" className="rounded-[20px]" />
           </div>
-          <div className="mt-4">
-            <h2 className="text-lg font-bold text-center">
-              {item.common_name}
-            </h2>{" "}
-            <p className="text-gray-600 text-center">${item.price}</p>{" "}
-            <p className="mt-2 text-sm text-gray-700">
-              Brand: <span className="text-blue-500">{item.brend}</span>
-            </p>{" "}
+          <div className="w-[90%] mx-auto flex justify-between">
+            <p>{item.common_name}</p>
+            <h2 className="cursor-pointer">${item.price}</h2>
+            <p className="cursor-pointer font-bold text-red-500">
+              Brend: <span className="text-blue-500">{item.brend}</span>
+            </p>
           </div>
         </div>
       ))}
